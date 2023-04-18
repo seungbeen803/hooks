24867fad8a8773c095e2d662339837f52383d945
seungbeen <seungbin803@gmail.com>
2023-04-18 14:46:27 +0900

    Add: Info1 주석

--- src/Info1.js
+++ src/Info1.js
@@ -1,10 +1,15 @@
 // Info1.js
 import { useReducer } from "react";
 
+// action = e.target
 function reducer (state, action) {
   return {
+    // state가 2개 이상 있을 때
     ...state,
+    // 
     [action.name]: action.value,
+    // 첫 번째 input박스에서 입력한 내용은 name state에 저장된다.
+    // 두 번째 input박스에서 입력한 내용은 nickname state에 저장된다.
   };
 }
 
@@ -13,14 +18,19 @@ const Info1 = () => {
     name: "",
     nickname: "",
   });
+  // name과 nickname은 state 변수로 선언
   const { name, nickname } = state;
   const onChange = (e) => {
+    // reducer함수가 호출, 인자 값은 e.target이 전달됨
     dispatch(e.target);
   };
 
+  // input박스가 많이 있어도 onChange이벤트 함수는 1개만 가지고 실행
   return(
     <div>
       <div>
+        {/* input박스에 데이터를 입력하면 Change이벤트가 발생하고
+        onChange함수를 호출 */}
         <input name="name" value={name} onChange={onChange} />
         <input name="nickname" value={nickname} onChange={onChange} />
       </div>
