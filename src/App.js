95a59465e53e0430b31705aa1d0cf021f42d4c78
seungbeen <seungbin803@gmail.com>
2023-04-21 10:55:39 +0900

    Add: useMemo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // App.js
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
-// const hardCalculate = (number) => {
-//   console.log('시간이 많이 걸리는 계산');
-//   for(let i = 0; i < 999999999; i++) {} // 1초 정도 지연
-//   return number + 10000;
-// };
+const hardCalculate = (number) => {
+  console.log('시간이 많이 걸리는 계산');
+  for(let i = 0; i < 999999999; i++) {} // 1초 정도 지연
+  return number + 10000;
+};
 
 const easyCalculate = (number) => {
   console.log('시간이 적게 걸리는 계산');
@@ -14,21 +14,30 @@ const easyCalculate = (number) => {
   return number + 1;
 };
 
-// state변수가 업데이트 될 경우 App() 컴포넌트는 처음부터 다시 렌더링이
+// easyNumber state변수가 업데이트 될 경우
+// App() 컴포넌트는 처음부터 다시 렌더링이
 // 발생되기 때문에 easyCalculate()함수를 호출하여도 자동적으로
 // hardCalculate()함수도 호출되기 때문에 시간이 지연된다.
 const App = () => {
   // hardNumber = 1
-  // const [hardNumber, setHardNumber] = useState(1);
+  const [hardNumber, setHardNumber] = useState(1);
   // easyNumber = 1
   const [easyNumber, setEasyNumber] = useState(1);
 
   // const hardSum = hardCalculate(hardNumber);
+
+  // hardCalculate()함수는 처음 렌더링할 때 호출이 된다.
+  // 메모이제이션을 하게 되면 [hardNumber]은 hardNumber값이
+  // 변경되지 않으면 hardCalculate()함수는 호출이 되지 않는다.
+  const hardSum = useMemo(() => {
+    return hardCalculate(hardNumber);
+  }, [hardNumber]);
+
   const easySum = easyCalculate(easyNumber);
 
   return (
     <>
-      {/* <div>
+      <div>
         <h3>시간이 많이 걸리는 계산</h3>
         <input
         type="number"
@@ -37,7 +46,7 @@ const App = () => {
         onChange={(e) => { setHardNumber(parseInt(e.target.value))}}
         />
         <span>+10000={hardSum}</span>
-      </div> */}
+      </div>
 
       <div>
         <h3>시간이 적게 걸리는 계산</h3>
