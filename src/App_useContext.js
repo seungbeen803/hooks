de28f3061234dd24f6ee294092458ccbba28b618
seungbeen <seungbin803@gmail.com>
2023-05-09 15:10:53 +0900

    Add: useContext 부모 한 개

---
+++ src/App_useContext.js
@@ -0,0 +1,26 @@
+//App.js
+function App() {
+  return <GrandParent value="Hello World!" />;
+}
+
+function GrandParent({ value }) {
+  return <Parent value={value} />;
+}
+
+function Parent({ value }) {
+  return <Child value={value} />;
+}
+
+function Child({ value }) {
+  return <GrandChild value={value} />;
+}
+
+function GrandChild({ value }) {
+  return <Message value={value} />;
+}
+
+function Message({ value }) {
+  return <div>Received: {value}</div>;
+}
+
+export default App;
